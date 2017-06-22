e605fed3e5bfcbbe9edbfd08cba266c63837b7ea
Boris Boutillier <boris.boutillier@gmail.com>
2017-06-22 22:20:23 +0200

    Arduino code to convert cmd_vel to motor, missing only real PID

--- arduino/MegaPi_ROS_CmdVel/MegaPi_ROS_CmdVel.ino
+++ arduino/MegaPi_ROS_CmdVel/MegaPi_ROS_CmdVel.ino
@@ -3,26 +3,31 @@
 // Subscribe to cmd_vel s a Twist message
 // Support parameters for robot base
 
+// TODO
+// Use personal PID, speedPID only do P.
+
 
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
 
 #include <MeMegaPi.h>
 
+#define PI 3.1415
+
 MeEncoderOnBoard Encoder1(SLOT1);
 MeEncoderOnBoard Encoder2(SLOT2);
 
 void interruptEncoder1(void) {
     if (digitalRead(Encoder1.getPortB())==0)
-        Encoder1.pulsePosMinus();
-    else
         Encoder1.pulsePosPlus();
+    else
+        Encoder1.pulsePosMinus();
 }
 void interruptEncoder2(void) {
     if (digitalRead(Encoder2.getPortB())==0)
-        Encoder2.pulsePosMinus();
-    else
         Encoder2.pulsePosPlus();
+    else
+        Encoder2.pulsePosMinus();
 }
 
 class NewHardware : public ArduinoHardware
@@ -32,51 +37,84 @@ class NewHardware : public ArduinoHardware
 };
 ros::NodeHandle_<NewHardware>  nh;
 
-long motors_timer;    // Handles stopping motors after certain time without message
-int  motors_timeout;
+long   motors_timer;    // Handles stopping motors after certain time without message
+int    motors_timeout;
+float  baseWidth;
+float  wheelRadius;
+
+char msg[128];
 
 void cmdVelCb( const geometry_msgs::Twist& cmd_vel_msg){
-    digitalWrite(13, HIGH-digitalRead(13));   // blink the led
-    nh.loginfo("Motors command received");
     motors_timer = millis() + motors_timeout;
+    nh.loginfo("Motor cmd_vel received");
+    float v = cmd_vel_msg.linear.x;
+    float w = cmd_vel_msg.angular.z;
+    float vL = v+(w*baseWidth)/2;
+    float vR = v-(w*baseWidth)/2;
+    float vLrpm = vL*30/(PI*wheelRadius);
+    float vRrpm = vR*30/(PI*wheelRadius);
+    sprintf(msg,"Current speed 1: %d vs %d 2: %d vs %d",
+        int(100*Encoder1.getCurrentSpeed())
+    ,   int(100*-vRrpm)
+    ,   int(100*Encoder2.getCurrentSpeed())
+    ,   int(100*vLrpm)
+    );
+    nh.loginfo(msg);
+    Encoder1.runSpeed(-vRrpm);
+    Encoder2.runSpeed(vLrpm);
+    motors_timer = millis()+motors_timeout;
 }
+
+ros::Subscriber<geometry_msgs::Twist> cmdVelSub("cmd_vel", &cmdVelCb);
 
 void setup()
 {
+    float pidConstants[3];
+
     nh.initNode();
+    nh.subscribe(cmdVelSub);
 
     attachInterrupt(Encoder1.getIntNum(), interruptEncoder1, RISING);
     attachInterrupt(Encoder2.getIntNum(), interruptEncoder2, RISING);
     
-    Encoder1.setMotorPwm(0);
+    // Get Node parameters
+    while (!nh.connected()) { nh.spinOnce(); };
+
+    if (!nh.getParam("~motors_timeout", &motors_timeout)) { 
+       nh.loginfo("Using default values for motors_timeout");
+       motors_timeout = 1000;
+    }
+    if (!nh.getParam("~pid", pidConstants, 3)) { 
+       nh.loginfo("Using default values for pid");
+       pidConstants[0] = 0.18;
+       pidConstants[1] = 0.01;
+       pidConstants[2] = 0;
+    }
+
+    if (!nh.getParam("~baseWidth", &baseWidth)) { 
+       baseWidth = 0.17;
+    }
+    if (!nh.getParam("~wheelRadius", &wheelRadius)) { 
+       wheelRadius = 0.032;
+    }
+
+    Encoder1.setSpeedPid(pidConstants[0],pidConstants[1],pidConstants[2]);
+    Encoder2.setSpeedPid(pidConstants[0],pidConstants[1],pidConstants[2]);
+
     Encoder1.setPulsePos(0);
-    Encoder1.setSpeedPid(0.18,0,0);
-    Encoder2.setMotorPwm(0);
     Encoder2.setPulsePos(0);
-    Encoder2.setSpeedPid(0.18,0,0);
 
     motors_timer = 0;
 
-    //while (!nh.connected()) { nh.spinOnce(); };
-    //if (!nh.getParam("~motors_timeout", &motors_timeout)) { 
-    //   nh.loginfo("Using default_values for motors_timeout");
-    //   motors_timeout = 1000;
-    //}
-    motors_timeout = 1000;
 }
 
 void loop() {
 
     if (motors_timer!=0 && millis()>motors_timer) {
-        Encoder1.setMotorPwm(0);
         Encoder1.runSpeed(0);
-        Encoder2.setMotorPwm(0);
+        Encoder2.runSpeed(0);
         motors_timer = 0;
-        nh.logdebug("Motors have been stopped after timeout");
-    }
-    if (millis()%10000==0) {
-        Encoder1.runSpeed(100.0);
-        motors_timer = millis() + motors_timeout;
+        nh.loginfo("Motors have been stopped after timeout");
     }
 
     Encoder1.loop();
