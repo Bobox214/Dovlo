f5b707df3f244a1baa022b51b980edfdebe5d532
Boris Boutillier <boris.boutillier@gmail.com>
2017-06-22 22:21:27 +0200

    Update launchers

--- raspberryPi/ros_arduino.sh
+++ raspberryPi/ros_arduino.sh
@@ -1,2 +1,2 @@
-rosrun rosserial_python serial_node.py /dev/serial0 serial_node:=rpi_serial_node &
+rosrun rosserial_python serial_node.py /dev/serial0 serial_node:=rpi_serial_node
 
--- raspberryPi/ros_raspberry_wifi.sh
+++ raspberryPi/ros_raspberry_wifi.sh
@@ -1,3 +1,4 @@
 export ROS_MASTER_URI=http://192.168.0.20:11311/
 export ROS_IP=192.168.0.20
 source `dirname ${BASH_SOURCE[0]}`/catkin_ws/devel/setup.bash
+roscore &
