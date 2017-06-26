dacaf90454aa50ce5e0823eb3a7f51e463f57a76
Boris Boutillier <boris.boutillier@gmail.com>
2017-06-26 21:52:01 +0200

    Update launchers to include common/

--- laptop/ros_xps_wifi.sh
+++ laptop/ros_xps_wifi.sh
@@ -1,4 +1,5 @@
-export ROS_MASTER_URI=http://192.168.0.24:11311/
+export ROS_MASTER_URI=http://192.168.0.23:11311/
 export ROS_IP=192.168.0.23
 source `dirname ${BASH_SOURCE[0]}`/../common/catkin_ws/devel/setup.bash
+roscore &
 
